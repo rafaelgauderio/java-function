5fb5002f17b6dcc91131fcaf57c7199f1bfb5ad3
Rafael De Luca <deluca1712@gmail.com>
2019-12-15 20:12:27 -0300

    lambdas expressions

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import Utilities.UpperCaseName;
@@ -24,19 +25,28 @@ public class Program {
 
 		List<String> namesUper = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 		namesUper.forEach(System.out::println);
-		
+
 		System.out.println();
 		System.out.println("Method reference static");
 		List<String> namesUpper = list.stream().map(Product::staticUpperCase).collect(Collectors.toList());
 		namesUpper.forEach(System.out::println);
-		
+
 		System.out.println();
-		System.out.println("Method reference nonstatic");
+		System.out.println("Method reference non static");
 		List<String> namesUpper2 = list.stream().map(Product::nosStaticUpperCase).collect(Collectors.toList());
 		namesUpper2.forEach(System.out::println);
-		
-		
-		
+
+		System.out.println();
+		System.out.println("Declared lambda expression");
+		Function<Product, String> func = pro -> pro.getName().toUpperCase();
+		List<String> namesLambda = list.stream().map(func).collect(Collectors.toList());
+		namesLambda.forEach(System.out::println);
+
+		System.out.println();
+		System.out.println("Inline lambda expresion");
+		List<String> namesLambdain = list.stream().map(pro -> pro.getName().toUpperCase()).collect(Collectors.toList());
+		namesLambdain.forEach(System.out::println);
+
 		sc.close();
 
 	}
